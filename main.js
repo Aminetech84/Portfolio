92a710f7a648fd91a307e6ebd26758106bd77596
Amine Ouldaouali <aminetech84@hotmail.com>
2019-06-22 16:09:57 +0100

    2

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+/*
+
+function active() {
+    let elem = document.querySelector('button');
+    console.log(elem.classList);
+
+    if (elem.classList.contains('1') === true) {
+        elem.addEventListener("click", () => {
+            elem.classList.add('active');
+            console.log(elem.classList);
+            
+    });
+        console.log(elem.classList);
+        elem.addEventListener("click", () => elem.classList.remove('active'));
+        console.log(elem.classList);
+    } else {
+        console.log(':(');
+    }
+    console.log(elem);
+}
+
+active();
+
+
+console.log(elem);
+/*
+
+function active() {
+    document.querySelector('button').classList.add("active");
+}
+
+document.querySelector('button').classList.add("active");
+
+
+
+
+if (elem.classList.contains('1') === true) {
+        document.addEventListener("click", () => document.getElementsByClassName('1').classList.add('active'));
+        console.log(elem);
+        
+    } else {
+        console.log(':(');
+    }
+    console.log(elem);
+}
+*/
